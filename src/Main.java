db874f92d63afe5e0fad8fad27cafe2120190eed
zgaskar <zeynepgaskar@gmail.com>
2026-05-25 18:21:12 +0300

    Update Main.java
    fix(main): handle NumberFormatException for tick input

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import models.Cell;
 public class Main {
     public static void main(String[] args) {
         if(args.length<2){
-            System.out.println("Error"); //error açıklamasını düzenlicem
+            System.out.println("Error! Usage: java -jar ObjectVilleGame.jar <mapFile.txt> <countOfTicks>"); //error açıklamasını düzenlicem
             return;
         }
-
+        int ticks = 0;
+        try{
+            ticks = Integer.parseInt(args[1]);
+        }catch(NumberFormatException e){
+            System.out.println("Error: Tick count must be an integer! ");
+            return;
+        }
         String mapFilePath = args[0];
-        int ticks = Integer.parseInt(args[1]);
+
 
         System.out.println("Map is loading: " + mapFilePath);
         System.out.println("Tick value: " + ticks);
@@ -19,6 +25,7 @@ public class Main {
         MapParser parser = new MapParser();
         parser.loadData(mapFilePath);
         CityGrid grid = parser.getCityGrid();
+        // TODO: 4. geliştiricinin Simulation sınıfı buraya eklenecek ve ticks değişkeni ona yollanacak
 
         for (int i = 0; i < grid.getRows(); i++) {
             for (int j = 0; j < grid.getCols(); j++) {
